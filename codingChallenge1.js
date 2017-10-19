fb5bf67f1225eb8ceabca1063fa1bc1e38531e6a
itsmerodolf <DotDot_12000@yahoo.com>
2017-10-19 20:13:43 +0800

    finished challenge

--- codingChallenge1.js
+++ codingChallenge1.js
@@ -17,3 +17,28 @@ need the && operator to take the decision. If you can't solve this one
 just watch the solution, it's no problem :)
 */
 
+var player1 = 'John';
+var player2 = 'Jeff';
+var player3 = 'Rodolf';
+
+var height1 = 190;
+var height2 = 170;
+var height3 = 176;
+
+var age1 = 27;
+var age2 = 24;
+var age3 = 30;
+
+var score1 = height1 + (5 * age1);
+var score2 = height2 + (5 * age2);
+var score3 = height3 + (5 * age3);
+
+if (score1 > score2 && score1 > score3) {
+	console.log(player1 + ' wins with the score of ' + score1 + ' points');
+} else if (score2 > score1 && score2 > score3) {
+	console.log(player2 + ' wins with the score of ' + score2 + ' points');
+} else if (score3 > score1 && score3 > score2) {
+	console.log(player3 + ' wins with the score of ' + score3 + ' points');
+} else {
+	console.log('There is a draw');	
+}
